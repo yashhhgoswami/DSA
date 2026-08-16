171fcc6da572d60794a0e80b7c5744132059fd63
Yash Goswami <yashhhgoswami@gmail.com>
2026-08-16 22:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 131.4 MB (33.50%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0, 0, 0};
+        for (int s : stones) {
+            cnt[s % 3]++;
+        }
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] >= 1 && cnt[2] >= 1;
+        } else {
+            return abs(cnt[1] - cnt[2]) > 2;
+        }
+    }
+};
