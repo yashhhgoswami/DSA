2fc33fe19a811749d054c3ada01d842cafa442b7
Yash Goswami <yashhhgoswami@gmail.com>
2026-09-03 23:12:18 +0530

    Time: 3 ms (2.30%) | Memory: 12.5 MB (18.49%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,14 +1,32 @@
 class Solution {
+private:
+    void swapIfGreater(vector<int>& nums1, vector<int>& nums2, int ind1, int ind2){
+        if(nums1[ind1] > nums2[ind2]){
+            swap(nums1[ind1], nums2[ind2]);
+        }
+    }
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        while (m > 0 && n > 0) {
-            if (nums1[m-1] > nums2[0]) {
-                swap(nums1[m-1], nums2[0]);
-                sort(nums1.begin(), nums1.begin() + m);
-                sort(nums2.begin(), nums2.begin() + n);
-            } else {
-                break;
+        int len = m + n;
+        int gap = (len/2) + (len%2);
+        while (gap > 0){
+            int left = 0;
+            int right = left + gap;
+            while (right < len){
+                if(left < m && right >= m){
+                    swapIfGreater(nums1, nums2, left, right - m);
+                }
+                else if(left >= m){
+                    swapIfGreater(nums2, nums2, left - m, right - m);
+                }
+                else {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+                left++;
+                right++;
             }
+            if (gap == 1) break;
+            gap = (gap/2) + (gap%2);
         }
         for (int i = 0; i < n; i++) {
             nums1[m + i] = nums2[i];
